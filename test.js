f47d6d0cf674b390cc7dd8caf125f65a0fc7d4f5
David Björklund <david.bjorklund@gmail.com>
2016-04-07 08:46:18 +0200

    update to ava@0.14

--- test.js
+++ test.js
@@ -23,7 +23,7 @@ test('works when adding properties', t => {
   const expected = from.set('beep', {boop: true});
   const actual = diff(from, to);
 
-  t.same(actual.asMutable(), expected.asMutable());
+  t.deepEqual(actual, expected);
   t.is(actual.foo, expected.foo);
 });
 
@@ -33,7 +33,7 @@ test('works when removing properties', t => {
   const expected = from.without('beep');
   const actual = diff(from, to);
 
-  t.same(actual.asMutable(), expected.asMutable());
+  t.deepEqual(actual, expected);
   t.is(actual.foo, expected.foo);
 });
 
@@ -52,7 +52,7 @@ test('works when chaging object to none-object', t => {
   const expected = from.set('foo', true);
   const actual = diff(from, to);
 
-  t.same(actual.asMutable(), expected.asMutable());
+  t.deepEqual(actual, expected);
   t.is(actual.foo, expected.foo);
 });
 
@@ -60,63 +60,63 @@ test('arrays, where it previous was something else', t => {
   const from = Immutable({foo: null});
   const to = {foo: [1, 2, 3]};
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('arrays, change a property', t => {
   const from = Immutable([{foo: 'bar'}, {hello: 'world'}]);
   const to = [{foo: 'bas'}, {hello: 'world'}];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('arrays, move a property', t => {
   const from = Immutable([{foo: 'bar'}, {hello: 'world'}]);
   const to = [{hello: 'world'}, {foo: 'bar'}];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('arrays move', t => {
   const from = Immutable([1, 2]);
   const to = [2, 1];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('arrays, reverse', t => {
   const from = Immutable([1, 2, 3, 4]);
   const to = [4, 3, 2, 1];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('arras, move from the end to the begining', t => {
   const from = Immutable([1, 2, 3, 4, 'foo']);
   const to = ['foo', 1, 2, 3, 4];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('arrays, update one value (special case)', t => {
   const from = Immutable([1, 2, {foo: {bar: true}}, 3, 4]);
   const to = [1, 2, {foo: {bar: true}, beep: 'boop'}, 3, 4];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
   t.is(actual[2].foo, from[2].foo);
 });
 
@@ -124,34 +124,34 @@ test('arrays, multiple adds', t => {
   const from = Immutable([1, 3]);
   const to = [1, 2, {foo: {bar: true}, beep: 'boop'}, 3, 4];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('equal array', t => {
   const to = [1, 2, {foo: {bar: true}, beep: 'boop'}, 3, 4];
   const from = Immutable(to);
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('array remove', t => {
   const from = Immutable([1, 2, 3]);
   const to = [1, 2];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('nested arrays', t => {
   const from = Immutable([[{nested: true}]]);
   const to = [[{nested: true}, {foo: 'bar'}]];
   const expected = to;
-  const actual = diff(from, to).asMutable();
+  const actual = diff(from, to);
 
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
